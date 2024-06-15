5c367e8f9624e7242c001fe34c8ad05745f78fd2
Noah Lin <noah.ai12@nycu.edu.tw>
2024-06-15 22:56:05 +0800

    inference modified

--- inference.py
+++ inference.py
@@ -323,12 +323,13 @@ def run_one_inference(img, corners, model, args, name, logger, show=False, show_
 
 
     dt['trivialWalls'][0] = wall_tw_tensor
+    save_name = name+"_pred.png"
 
     visualize_2d(img, dt,
                 show_depth=show_depth,
                 show_floorplan=show_floorplan,
                 show=show,
-                save_path=os.path.join(args.output_dir, f"{name}_pred.png"))
+                save_path=os.path.join(args.output_dir, save_name)
 
     
     # temporary, need to integrate to save_pred_json
